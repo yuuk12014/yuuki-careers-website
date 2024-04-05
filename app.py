fb833e92e2b2c20ad868705418feef334584c7bf
yuuk12014 <recar_2001@yahoo.com>
2024-04-05 04:51:42 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, jsonify, request
 
 app = Flask(__name__)
 
+CONTACTS = [{
+  'id': 1,
+  'FName': 'John',
+  'LName': 'Doe',
+  'Emailaddress': 'johndoe@gmail.com',
+  'Address': '123 Main St, Anytown, USA'
+},
+{
+  'id': 2,
+  'FName': 'Jim',
+  'LName': 'Doe',
+  'Emailaddress': 'johndoe@gmail.com',
+  'Address': '123 Main St, Anytown, USA'
+},
+{
+  'id': 3,
+  'FName': 'Jazz',
+  'LName': 'Doe',
+  'Emailaddress': 'johndoe@gmail.com',
+  'Address': '123 Main St, Anytown, USA'
+},
+{
+  'id': 4,
+  'FName': 'John',
+  'LName': 'Doe',
+  'Emailaddress': 'johndoe@gmail.com',
+  'Address': '123 Main St, Anytown, USA'
+            }
+]
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def homepage():
+  return render_template('index.html',
+    contacts = CONTACTS)
+
+@app.route('/contacts')
+def contacts():
+  return jsonify(CONTACTS)
   
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
